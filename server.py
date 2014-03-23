364b100cb7843a0c481728843c333a8c5abfd4d0
Adam <agillfi@ncsu.edu>
2014-03-23 15:06:53 -0400

    added function to create rfc_list

--- server.py
+++ server.py
@@ -20,11 +20,28 @@ def create_peer_list(dictionary_list, hostname, port):
     dictionary_list.insert(0, dict(zip(keys, entry)))
     return peer_list, keys
 
+example_dict_list_of_rfcs = [{'RFC Number': 1234, 'RFC Title': "This is an RFC Title"},
+                             {'RFC Number': 4321, 'RFC Title': "This is another RFC Title"}]
+
+
+#Creates RFC_list
+def create_rfc_list(dictionary_list, dict_list_of_rfcs, hostname):
+    keys = ['RFC Number', 'RFC Title', 'Hostname']
+
+    for rfc in dict_list_of_rfcs:
+        rfc_number = rfc['RFC Number']
+        rfc_title = rfc['RFC Title']
+        entry = [rfc_number, rfc_title, hostname]
+        dictionary_list.insert(0, dict(zip(keys, entry)))
+
+    return RFC_list, keys
+
 
 #Prints the list of dictionary items
 def print_dictionary(dictionary_list, keys):
     for item in dictionary_list:
         print(' '.join([item[key] for key in keys]))
+
 
 while True:
     c, addr = s.accept()     # Establish connection with client.
