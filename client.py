94ba45aea763038f7da3f5b148d998d5ede9f1ab
Adam Gillfillan <agillfi@ncsu.edu>
2014-04-02 10:14:54 -0400

    cleaned code up a bit by meeting PEP 8 guidelines

--- client.py
+++ client.py
@@ -24,12 +24,13 @@ import pickle
 upload_port_num = 7777
 dict_list_of_rfcs = []  #list of dictionaries of RFC numbers and Titles.
 
+
 # display p2p response message
 def p2p_response_message(rfc_num): # the parameter "rfc_num" should be str
     filename = "rfc"+str(rfc_num)+".txt"
     current_time = time.strftime("%a, %d %b %Y %X %Z", time.localtime())
     OS = platform.platform()
-    if(os.path.exists(filename) == 0):
+    if os.path.exists(filename) == 0:
         status = "404"
         phrase = "Not Found"
         message = "P2P-CI/1.0 "+ status + " "+ phrase + "\n"\
@@ -43,69 +44,71 @@ def p2p_response_message(rfc_num): # the parameter "rfc_num" should be str
         last_modified = time.ctime(os.path.getmtime(filename))
         content_length = os.path.getsize(filename)
         message	= "P2P-CI/1.0 "+ status + " "+ phrase + "\n"\
-                  "Date: "+ current_time + "\n"\
-                    "OS: "+str(OS)+"\n"\
+                  "Date: " + current_time + "\n"\
+                  "OS: " + str(OS)+"\n"\
                   "Last-Modified: " + last_modified + "\n"\
                   "Content-Length: " + str(content_length) + "\n"\
                   "Content-Type: text/text \n"\
-                   +str(data)
+                  + str(data)
     #print message
     return message
 
+
 # send rfcs to peers
 def send_file(filename):  # send the RFC to peers
-    
-
     txt = open(filename)
     data = txt.read(1024)
-    while(data):
+    while data:
         s.send(data)
-        data =txt.read(1024)
+        data = txt.read(1024)
     s.close()
+
 
 # display p2p request message
 def p2p_request_message(rfc_num, host):
-	OS = platform.platform()
-	message = "GET RFC "+str(rfc_num)+" P2P-CI/1.0 \n"\
-		      "Host: "+str(host)+"\n"\
-		      "OS: "+str(OS)+"\n"
-	#print message
-	return message
+    OS = platform.platform()
+    message = "GET RFC "+str(rfc_num)+" P2P-CI/1.0 \n"\
+              "Host: "+str(host)+"\n"\
+              "OS: "+str(OS)+"\n"
+    #print message
+    return message
+
 
 # display p2s request message for ADD and LOOKUP methods
 def p2s_request_message(rfc_num, host, port, method, title):# for ADD, LOOKUP method
-	message = method + " RFC " +str(rfc_num)+" P2P-CI/1.0 \n"\
-			  "Host: "+str(host)+"\n"\
-			  "Port: "+str(port)+"\n"\
-			  "Title: "+str(title)+"\n"
-	#print message
-	#return message
+    message = method + " RFC " + str(rfc_num)+" P2P-CI/1.0 \n"\
+              "Host: " + str(host)+"\n"\
+              "Port: " + str(port)+"\n"\
+              "Title: " + str(title)+"\n"
+    #print message
+    #return message
 
 
 #display p2s request message for LIST methods
 def p2s_list_request(host, port):
-	message = "LIST ALL P2P-CI/1.0 \n"\
-			  "Host: "+str(host)+"\n"\
-			  "Port: "+str(port)+"\n"
-	#print message
-	return message
+    message = "LIST ALL P2P-CI/1.0 \n"\
+              "Host: "+str(host)+"\n"\
+              "Port: "+str(port)+"\n"
+    #print message
+    return message
+
 
 #get the list of the local rfcs
 def get_local_rfcs():
-	rfcs_path = os.getcwd() + "/rfc"
-	rfcs_num =[num[num.find("c")+1:num.find(".")] for num in os.listdir(rfcs_path) if 'rfc' in num]
-	return rfcs_num
+    rfcs_path = os.getcwd() + "/rfc"
+    rfcs_num =[num[num.find("c")+1:num.find(".")] for num in os.listdir(rfcs_path) if 'rfc' in num]
+    return rfcs_num
+
 
 #pass peer's hostname, port number and rfc_num, rfc_title
-
 def peer_infomation():
-	keys = ["RFC Number", "RFC Title"]
-	rfcs_num = get_local_rfcs()
-	rfcs_title = ["title1", "title2", "title3"]
-	for num, title in zip(rfcs_num, rfcs_title):
-		entry = [num, title]
-		dict_list_of_rfcs.insert(0, dict(zip(keys, entry)))
-	return [upload_port_num, dict_list_of_rfcs]  #[port, rfcs_num, rfcs_title]
+    keys = ["RFC Number", "RFC Title"]
+    rfcs_num = get_local_rfcs()
+    rfcs_title = ["title1", "title2", "title3"]
+    for num, title in zip(rfcs_num, rfcs_title):
+        entry = [num, title]
+        dict_list_of_rfcs.insert(0, dict(zip(keys, entry)))
+    return [upload_port_num, dict_list_of_rfcs]  #[port, rfcs_num, rfcs_title]
 
 
 s=socket.socket()          # Create a socket object
@@ -120,15 +123,13 @@ data = s.recv(1024)
 print(data.decode('utf-8'))
 
 
-
 #if key press, then close:
 user_input = input("> Enter 1 for exit: ")
 if user_input == "1":
     s.send(bytes('1', "utf-8"))
     s.close                     # Close the socket when done
 elif user_input == "2":
-     pass
-
+    pass
 else:
     user_input = input("> Enter 1 for exit: ")
 
