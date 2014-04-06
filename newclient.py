fd0f9a7051c444fcb3ff6e61bbfe8112a5822753
WeiFoo <wfu@ncsu.edu>
2014-04-06 14:13:33 -0400

    update

--- newclient.py
+++ newclient.py
@@ -6,14 +6,26 @@ import platform				# Import platform module to get our OS
 import os
 from _thread import *
 import pickle
-
-s = socket.socket()# Create a socket object
+def p2p_downlaod_request(rfc_num):
+    s = socket.socket() # Create a socket object
 #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEDADDR, 1)
-host = socket.gethostname()  # Get local machine name
-port = 65283                # You need to modify this port number according to the client
-s.connect((host,port))
-data = "GET RFC 1 P2P-CI/1.0"
-s.send(bytes(data, 'utf-8'))
-data_rec = s.recv(1024)
-print (data_rec.decode('utf-8'))
-s.close()
+    host = socket.gethostname()  # Get local machine name
+    port = 65055             # You need to modify this port number according to the client
+    s.connect((host, port))
+    rfc_num = "1"
+    data = "GET RFC "+rfc_num+" P2P-CI/1.0"
+    s.send(bytes(data, 'utf-8'))
+    data_rec = s.recv(1024)
+    print (data_rec.decode('utf-8'))
+    current_path = os.getcwd()
+    filename = "rfc"+rfc_num+".txt"
+    OS = platform.system()
+    if OS == "Windows":  # determine rfc path for two different system
+        filename = current_path + "\\rfc\\" + filename
+    else:
+        filename = current_path + "/rfc/" + filename
+    f = open(filename,'w')
+    f.write(data_rec.decode('utf-8'))
+    f.close()
+    s.close()
+p2p_downlaod_request(str(1))
